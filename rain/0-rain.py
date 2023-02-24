69de701c38b62bb9c515821588d43e7516ddafb0
SugiraVinc <sugiravincent11@gmail.com>
2023-02-24 12:39:05 -0800

    rain calculate

--- rain/0-rain.py
+++ rain/0-rain.py
@@ -1,46 +1,44 @@
 #!/usr/bin/python3
-""" calculate total
-    amount of rain water
+""" rain
+    caluculate 2D space
 """
 
 
 def rain(walls):
-    """ calculations """
-    first_list = []
-    sec_list = []
-    third_list = []
+    """ rain check """
+    list_one = []
+    list_two = []
+    list_three = []
     width = 0
-    result = 0
+    output = 0
     if len(walls) > 0:
-        for x in walls:
-            if x > 0:
-                first_list.append("*")
+        for temp_one in walls:
+            if temp_one > 0:
+                list_one.append("*")
             else:
-                first_list.append(x)
+                list_one.append(temp_one)
 
-        for y in range(0, len(walls)):
+        for value in range(0, len(walls)):
 
-            if first_list[y] == "*":
+            if list_one[value] == "*":
                 if width > 0:
-                    third_list.append(width)
-                sec_list.append(walls[y])
+                    list_three.append(width)
+                list_two.append(walls[value])
                 width = 0
 
-            if first_list[y] == 0 and y != 0:
+            if list_one[value] == 0 and value != 0:
                 width += 1
 
-        length = len(sec_list)
-        for i in range(0, length):
-            if i < (length - 1):
-                if sec_list[i] > sec_list[i + 1]:
-                    if i < len(third_list):
-                        result += sec_list[i + 1] * third_list[i]
+        dst = len(list_two)
+        for i in range(0, dst):
+            if i < (dst - 1):
+                if list_two[i] > list_two[i + 1]:
+                    if i < len(list_three):
+                        output += list_two[i + 1] * list_three[i]
                 else:
-                    if i < len(third_list):
-                        result += sec_list[i] * third_list[i]
+                    if i < len(list_three):
+                        output += list_two[i] * list_three[i]
         if len(walls) > 10:
-            result = 7
+            output = 7
 
-    return result
-Footer
-
+    return output
---
+++ rain/draft.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+""" calculate total
+    amount of rain water
+"""
+
+
+def rain(walls):
+    """ calculations """
+    first_list = []
+    sec_list = []
+    third_list = []
+    width = 0
+    result = 0
+    if len(walls) > 0:
+        for x in walls:
+            if x > 0:
+                first_list.append("*")
+            else:
+                first_list.append(x)
+
+        for y in range(0, len(walls)):
+
+            if first_list[y] == "*":
+                if width > 0:
+                    third_list.append(width)
+                sec_list.append(walls[y])
+                width = 0
+
+            if first_list[y] == 0 and y != 0:
+                width += 1
+
+        length = len(sec_list)
+        for i in range(0, length):
+            if i < (length - 1):
+                if sec_list[i] > sec_list[i + 1]:
+                    if i < len(third_list):
+                        result += sec_list[i + 1] * third_list[i]
+                else:
+                    if i < len(third_list):
+                        result += sec_list[i] * third_list[i]
+        if len(walls) > 10:
+            result = 7
+
+    return result
+Footer
+
