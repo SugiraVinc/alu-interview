534d569485526408bfc29a175d57c0186a40074f
SugiraVinc <sugiravincent11@gmail.com>
2023-02-24 12:59:01 -0800

    rain calculate

--- rain/0-rain.py
+++ rain/0-rain.py
@@ -7,8 +7,8 @@
 def rain(walls):
     """ calculations for rain """
     first_list = []
-    list_two = []
-    list_three = []
+    sec_list = []
+    third_list = []
     width = 0
     result = 0
     if len(walls) > 0:
@@ -22,22 +22,22 @@ def rain(walls):
 
             if first_list[y] == "*":
                 if width > 0:
-                    list_three.append(y)
-                list_two.append(walls[y])
+                    third_list.append(y)
+                sec_list.append(walls[y])
                 width = 0
 
             if first_list[y] == 0 and y != 0:
                 width += 1
 
-        length = len(list_two)
-        for i in range(0, dst):
+        length = len(sec_list)
+        for i in range(0, length):
             if i < (lenght - 1):
-                if list_two[i] > list_two[i + 1]:
-                    if i < len(list_three):
-                        result += list_two[i + 1] * list_three[i]
+                if sec_list[i] > sec_list[i + 1]:
+                    if i < len(third_list):
+                        result += sec_list[i + 1] * third_list[i]
                 else:
-                    if i < len(list_three):
-                        result += list_two[i] * list_three[i]
+                    if i < len(third_list):
+                        result += sec_list[i] * third_list[i]
         if len(walls) > 10:
             output = 7
 
