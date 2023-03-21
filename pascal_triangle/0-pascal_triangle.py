e8d433b8b3cae394fe768a46daf9f93aff4cf884
SugiraVinc <sugiravincent11@gmail.com>
2023-03-21 16:10:51 +0000

    pascal triangle

--- pascal_triangle/0-pascal_triangle.py
+++ pascal_triangle/0-pascal_triangle.py
@@ -9,7 +9,7 @@ def pascal_triangle(n):
     if n <= 0:
         return []
     else:
-    	for i in range(1, n+1):
+        for i in range(1, n+1):
             x = 1
             oglist = []
             for j in range(1, i+1):
