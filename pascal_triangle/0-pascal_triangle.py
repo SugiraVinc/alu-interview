6379291d1e0756b3aab58e7c38705ed13b3021cc
SugiraVinc <sugiravincent11@gmail.com>
2023-03-21 11:15:09 +0000

    pascal triangle

---
+++ pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Pascal's triangle
+"""
+
+list = [1]
+def pascal_triangle(n):
+    for i in range (n)
+	print(list)
+	oglist = []
+	oglist.append(list[0])
+	for i in range(len(list)-1):
+	    oglist.append(list[i]+list[i+1])
+	oglist.append(list[-1])
+	list = oglist
+	return list
